50315fb156d7f0434e3182759ee1ecf97f061ac0
Bruno S Rodrigues <34436334+brusangues@users.noreply.github.com>
2021-09-29 15:31:29 -0300

    2.3. maincpp 2

--- examples/firstapp/main.cpp
+++ examples/firstapp/main.cpp
@@ -2,15 +2,19 @@
 #include "openglwindow.hpp"//openglwindow class definition
 
 int main(int argc, char **argv) {
-  // Create application instance
-  abcg::Application app(argc, argv); // app management
-  
-  // Create OpenGL window
-  auto window{std::make_unique<OpenGLWindow>()};// smart pointer creation
-  window->setWindowSettings({.title = "First App"});// window title definition; 
-  
-  // Run application
-  app.run(std::move(window));// abcg::Application::run is called
-  
+  try {
+    // Create application instance
+    abcg::Application app(argc, argv); // app management
+    
+    // Create OpenGL window
+    auto window{std::make_unique<OpenGLWindow>()};// smart pointer creation
+    window->setWindowSettings({.title = "First App"});// window title definition; 
+    
+    // Run application
+    app.run(std::move(window));// abcg::Application::run is called
+  } catch (const abcg::Exception &exception) {
+    fmt::print(stderr, "{}\n", exception.what());
+    return -1;
+  }
   return 0;
 }
