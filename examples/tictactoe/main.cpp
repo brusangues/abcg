b4badcb334a08a2c32b84583a963a011d3717f64
Bruno S Rodrigues <34436334+brusangues@users.noreply.github.com>
2021-10-30 20:07:33 -0300

    tictactoe project start

---
+++ examples/tictactoe/main.cpp
@@ -0,0 +1,21 @@
+#include <fmt/core.h>
+#include "abcg.hpp"
+#include "openglwindow.hpp"//openglwindow class definition
+
+int main(int argc, char **argv) {
+  try {
+    // Create application instance
+    abcg::Application app(argc, argv); // app management
+    
+    // Create OpenGL window
+    auto window{std::make_unique<OpenGLWindow>()};// smart pointer creation
+    window->setWindowSettings({.title = "First App"});// window title definition; 
+    
+    // Run application
+    app.run(std::move(window));// abcg::Application::run is called
+  } catch (const abcg::Exception &exception) {
+    fmt::print(stderr, "{}\n", exception.what());
+    return -1;
+  }
+  return 0;
+}
---
+++ examples/tictactoe/openglwindow.hpp
@@ -0,0 +1,19 @@
+#ifndef OPENGLWINDOW_HPP_
+#define OPENGLWINDOW_HPP_
+
+#include <array>
+
+#include "abcg.hpp"
+
+class OpenGLWindow : public abcg::OpenGLWindow {
+ protected:
+  void initializeGL() override; // override optional but recommended
+  void paintGL() override;
+  void paintUI() override;
+
+ private:
+  std::array<float, 4> m_clearColor{0.906f, 0.910f, 0.918f, 1.0f};
+  // 4 position array defining the background color
+};
+
+#endif
