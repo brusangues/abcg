d3bd9b6571e6ea42dfcf42882df648c709c91739
Bruno S Rodrigues <34436334+brusangues@users.noreply.github.com>
2021-11-01 15:48:35 -0300

    sierpinski main.cpp

---
+++ examples/sierpinski/main.cpp
@@ -0,0 +1,30 @@
+#include <fmt/core.h>
+#include "abcg.hpp"
+#include "openglwindow.hpp"//openglwindow class definition
+
+int main(int argc, char **argv) {
+  try {
+    // Create application instance
+    abcg::Application app(argc, argv); // app management
+    
+    // Create OpenGL window
+    auto window{std::make_unique<OpenGLWindow>()};// smart pointer creation
+    window->setOpenGLSettings(
+        {.samples = 2, //to support antialiasing
+         .preserveWebGLDrawingBuffer = true // framebuffer not erased between frames
+    });
+    //used by SDL in the opengl context creation - which represents a framebuffer linked to the window
+
+    window->setWindowSettings({.width = 600,
+                               .height = 600,
+                               .showFullscreenButton = false, // f11 still works
+                               .title = "Sierpinski Triangle"});
+    
+    // Run application
+    app.run(std::move(window));// abcg::Application::run is called
+  } catch (const abcg::Exception &exception) {
+    fmt::print(stderr, "{}\n", exception.what());
+    return -1;
+  }
+  return 0;
+}
